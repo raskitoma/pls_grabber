04b20fd7d4fe38a0327114c7192cdbd219356bb2
Elias Achi <elias@plazacybersecurity.com>
2023-05-31 21:11:46 -0500

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "==================== Starting PLS Data Grabber..."
-echo "==================== Setting up cron..."
+echo "==================== Setting up cron task..."
 
 # Set up the cron job
-echo "*/$CRON_SCHEDULE /usr/local/bin/python /app/plsgrabber.py > /proc/1/fd/1 2>&1" | crontab -
+echo "$CRON_SCHEDULE /usr/local/bin/python /app/plsgrabber.py > /proc/1/fd/1 2>&1" | crontab -
 crontab -l
 
-echo "==================== Starting cron..."
+echo "==================== Task created..."
 
 # Start cron in the foreground
 exec cron -f
