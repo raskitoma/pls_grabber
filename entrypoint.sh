3f7f1a990738935299c25c298be2fb519cd47248
Elias Achi <elias@plazacybersecurity.com>
2023-06-01 12:33:28 -0500

    Fixes PATH env

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,11 @@ env | grep INFLUX_HOST_PORT
 env | grep INFLUX_BUCKET
 env | grep PLS_
 env | grep CRON_
+env | grep PATH
 
 echo "==================== Setting up cron task..."
 # Set up the cron job
-echo "$CRON_SCHEDULE env INFLUX_HOST_PORT=$INFLUX_HOST_PORT INFLUX_ORG=$INFLUX_ORG INFLUX_BUCKET=$INFLUX_BUCKET INFLUX_KEY=$INFLUX_KEY PLS_PRICE_URI=$PLS_PRICE_URI PLS_LAUNCH_URI=$PLS_LAUNCH_URI /usr/local/bin/python /app/plsgrabber.py > /proc/1/fd/1 2>&1" | crontab -
+echo "$CRON_SCHEDULE env PATH=$PATH:/usr/bin:/opt env INFLUX_HOST_PORT=$INFLUX_HOST_PORT INFLUX_ORG=$INFLUX_ORG INFLUX_BUCKET=$INFLUX_BUCKET INFLUX_KEY=$INFLUX_KEY PLS_PRICE_URI=$PLS_PRICE_URI PLS_LAUNCH_URI=$PLS_LAUNCH_URI /usr/local/bin/python /app/plsgrabber.py > /proc/1/fd/1 2>&1" | crontab -
 
 echo "==================== Task created..."
 crontab -l
