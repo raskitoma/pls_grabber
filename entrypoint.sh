4eac14995fad7114e5e08211722d0ce1b5b15440
Elias Achi <elias@plazacybersecurity.com>
2023-06-07 19:49:57 -0500

    Fixing crontab config

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,7 @@ env | grep PATH
 
 echo "==================== Setting up cron task for pls data bunch and price grab..."
 # Set up the cron job
-echo "$CRON_SCHEDULE env PATH=$PATH:/usr/bin:/opt env INFLUX_HOST_PORT=$INFLUX_HOST_PORT INFLUX_ORG=$INFLUX_ORG INFLUX_BUCKET=$INFLUX_BUCKET INFLUX_KEY=$INFLUX_KEY PLS_PRICE_URI=$PLS_PRICE_URI PLS_PRICE_API_KEY=$PLS_PRICE_API_KEY PLS_LAUNCH_URI=$PLS_LAUNCH_URI /usr/local/bin/python /app/plsgrabber.py > /proc/1/fd/1 2>&1 \
-$CRON_PRICE env PATH=$PATH:/usr/bin:/opt env INFLUX_HOST_PORT=$INFLUX_HOST_PORT INFLUX_ORG=$INFLUX_ORG INFLUX_BUCKET=$INFLUX_BUCKET INFLUX_KEY=$INFLUX_KEY PLS_PRICE_URI=$PLS_PRICE_URI PLS_PRICE_API_KEY=$PLS_PRICE_API_KEY PLS_LAUNCH_URI=$PLS_LAUNCH_URI /usr/local/bin/python /app/pls_price_grabber.py > /proc/1/fd/1 2>&1" | crontab -
+printf "%s env PATH=$PATH:/usr/bin:/opt env INFLUX_HOST_PORT=$INFLUX_HOST_PORT INFLUX_ORG=$INFLUX_ORG INFLUX_BUCKET=$INFLUX_BUCKET INFLUX_KEY=$INFLUX_KEY PLS_PRICE_URI=$PLS_PRICE_URI PLS_PRICE_API_KEY=$PLS_PRICE_API_KEY PLS_LAUNCH_URI=$PLS_LAUNCH_URI /usr/local/bin/python /app/plsgrabber.py > /proc/1/fd/1 2>&1\n%s env PATH=$PATH:/usr/bin:/opt env INFLUX_HOST_PORT=$INFLUX_HOST_PORT INFLUX_ORG=$INFLUX_ORG INFLUX_BUCKET=$INFLUX_BUCKET INFLUX_KEY=$INFLUX_KEY PLS_PRICE_URI=$PLS_PRICE_URI PLS_PRICE_API_KEY=$PLS_PRICE_API_KEY PLS_LAUNCH_URI=$PLS_LAUNCH_URI /usr/local/bin/python /app/pls_price_grabber.py > /proc/1/fd/1 2>&1" "$CRON_SCHEDULE" "$CRON_PRICE" | crontab -
 
 echo "==================== Tasks created..."
 crontab -l
